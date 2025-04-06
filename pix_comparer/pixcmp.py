58bc71bd25728f3b85e22ec89b75a4bea32ec913
Rafal <raf88pogo@gmail.com>
2025-04-06 20:31:10 +0200

    add resize_to_match function

--- pix_comparer/pixcmp.py
+++ pix_comparer/pixcmp.py
@@ -1,12 +1,20 @@
 from PIL import Image, ImageChops
 
 
+def resize_to_match(img1, img2):
+    size = (max(img1.width, img2.width), max(img1.height, img2.height))
+    return img1.resize(size), img2.resize(size)
+
+
 def compare_images(img1_path, img2_path, save_path):
     img1 = Image.open(img1_path).convert("RGB")
     img2 = Image.open(img2_path).convert("RGB")
 
-    diff = ImageChops.difference(img1, img2)
-    diff_pixels = sum(1 for px in diff.getdata() if px != (0,0,0))
+    if img1.size != img2.size:
+        img1, img2 = resize_to_match(img1, img2)
+
+    #diff = ImageChops.difference(img1, img2)
+    #diff_pixels = sum(1 for px in diff.getdata() if px != (0,0,0))
 
     # RGB 255,0,0 czerwony kolor
     highlight = Image.new("RGB", img1.size)
@@ -16,11 +24,11 @@ def compare_images(img1_path, img2_path, save_path):
             ph[x, y] = (255, 0, 0) if p1[x, y] != p2[x, y] else p1[x, y]
     highlight.save(save_path)
 
-    print(f"Found {diff_pixels} differing pixels.")
+    #print(f"Found {diff_pixels} differing pixels.")
 
 img1 = "C:/Users/raf88/Desktop/pix-comparer/temp_img/img1.JPG"
 img2 = "C:/Users/raf88/Desktop/pix-comparer/temp_img/img2.JPG"
-save = "C:/Users/raf88/Desktop/pix-comparer/temp_img"
+save = "C:/Users/raf88/Desktop/pix-comparer/temp_img/diff.JPG"
 compare_images(img1, img2, save)
 
 
