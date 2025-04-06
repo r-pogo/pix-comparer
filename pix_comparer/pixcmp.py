2245af2cef5e6d1bf8d83bcd5a5e7340c1dc24c4
Rafal <raf88pogo@gmail.com>
2025-04-06 20:37:50 +0200

    add side by side comparison

--- pix_comparer/pixcmp.py
+++ pix_comparer/pixcmp.py
@@ -6,15 +6,12 @@ def resize_to_match(img1, img2):
     return img1.resize(size), img2.resize(size)
 
 
-def compare_images(img1_path, img2_path, save_path):
+def compare_images(img1_path, img2_path, save_path, side_path):
     img1 = Image.open(img1_path).convert("RGB")
     img2 = Image.open(img2_path).convert("RGB")
 
     if img1.size != img2.size:
         img1, img2 = resize_to_match(img1, img2)
-
-    #diff = ImageChops.difference(img1, img2)
-    #diff_pixels = sum(1 for px in diff.getdata() if px != (0,0,0))
 
     # RGB 255,0,0 czerwony kolor
     highlight = Image.new("RGB", img1.size)
@@ -24,12 +21,20 @@ def compare_images(img1_path, img2_path, save_path):
             ph[x, y] = (255, 0, 0) if p1[x, y] != p2[x, y] else p1[x, y]
     highlight.save(save_path)
 
-    #print(f"Found {diff_pixels} differing pixels.")
+    # Side-by-side image
+    combined = Image.new("RGB", (img1.width * 3, img1.height))
+    combined.paste(img1, (0, 0))
+    combined.paste(img2, (img1.width, 0))
+    combined.paste(highlight, (img1.width * 2, 0))
+    combined.save(side_path)
+
+
 
 img1 = "C:/Users/raf88/Desktop/pix-comparer/temp_img/img1.JPG"
 img2 = "C:/Users/raf88/Desktop/pix-comparer/temp_img/img2.JPG"
 save = "C:/Users/raf88/Desktop/pix-comparer/temp_img/diff.JPG"
-compare_images(img1, img2, save)
+side = "C:/Users/raf88/Desktop/pix-comparer/temp_img/diff2.JPG"
+compare_images(img1, img2, save, side)
 
 
 
