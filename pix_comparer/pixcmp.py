d3a76eb174ebf83e1e0fc02266ca29fc5608cef8
Rafal <raf88pogo@gmail.com>
2025-04-06 20:54:41 +0200

    add tolerance calculation for diff highlighting

--- pix_comparer/pixcmp.py
+++ pix_comparer/pixcmp.py
@@ -13,12 +13,24 @@ def compare_images(img1_path, img2_path, save_path, side_path):
     if img1.size != img2.size:
         img1, img2 = resize_to_match(img1, img2)
 
-    # RGB 255,0,0 czerwony kolor
+
     highlight = Image.new("RGB", img1.size)
     p1, p2, ph = img1.load(), img2.load(), highlight.load()
+    tolerance = 110
     for y in range(img1.height):
         for x in range(img1.width):
-            ph[x, y] = (255, 0, 0) if p1[x, y] != p2[x, y] else p1[x, y]
+            r1, g1, b1 = p1[x, y]
+            r2, g2, b2 = p2[x, y]
+            # Calculate diff for each color channel
+            diff_r = abs(r1 - r2)
+            diff_g = abs(g1 - g2)
+            diff_b = abs(b1 - b2)
+            # RGB 255,0,0 czerwony kolor
+            if diff_r > tolerance or diff_g > tolerance or diff_b > tolerance:
+                ph[x, y] = (255, 0, 0)
+            else:
+                ph[x, y] = p1[x, y]
+
     highlight.save(save_path)
 
     # Side-by-side image
